39fa205f34b9ff0da664ec5e6fd70e905d823631
Mike Wu <hello@mikewu.dev>
2022-11-13 23:40:25 +0900

    Set @AndroidEntryPoint on main activity

--- app/src/main/java/dev/protrack/uitestapp/MainActivity.kt
+++ app/src/main/java/dev/protrack/uitestapp/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import dev.protrack.uitestapp.ui.theme.UITestAppTheme
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
