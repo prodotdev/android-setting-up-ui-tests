6a0d191803062f523de4780cac2cc98a5169b26c
Mike Wu <hello@mikewu.dev>
2022-11-14 20:55:36 +0900

    Add TestModules to inject in tests

---
+++ app/src/androidTest/java/dev/protrack/uitestapp/TestModules.kt
@@ -0,0 +1,22 @@
+package dev.protrack.uitestapp
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [
+        AuthModules::class
+    ]
+)
+object TestModules {
+    @Provides
+    @Singleton
+    fun provideAuthService(): AuthService {
+        return AuthService()
+    }
+}
